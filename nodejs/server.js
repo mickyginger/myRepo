7157637787197683869d56c4e712bf54a9a20bb5
Mike Hayden <mickyginger@gmail.com>
2013-02-21 10:58:34 +0000

    Finished node.js tutorial

--- nodejs/server.js
+++ nodejs/server.js
@@ -5,7 +5,9 @@ function start(route, handle){
 	http.createServer(function(request, response){
 		var pathname = url.parse(request.url).pathname;
 		console.log('request for ' + pathname + ' received.');
-		route(handle, pathname, response);
+
+		route(handle, pathname, response, request);
+
 	}).listen(8080);
 
 	console.log('server has started.');
